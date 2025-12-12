fd392c7d16cdb900d3d3d3ca11fd7fc7f4664866
小末 <16186658+enderman-bm@user.noreply.gitee.com>
2025-12-12 20:42:25 +0800

    Add: 占位欢迎语  - 在lumino->main.rs中增加欢迎语打印

--- Lumino/src/main.rs
+++ Lumino/src/main.rs
@@ -1,3 +1,5 @@
+const LUMINO_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! Hello Lumino {}!", LUMINO_VERSION); //这句是我写的awa
 }
